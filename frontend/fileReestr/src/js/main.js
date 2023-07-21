f2bdd50fa88a2ebd59306f61da4d62872e4e8d06
kropalikhappines <arsik30394@gmail.com>
2023-07-21 16:47:36 +0300

    Добавление меню папок

--- frontend/fileReestr/src/js/main.js
+++ frontend/fileReestr/src/js/main.js
@@ -1,7 +1,25 @@
-const button = document.querySelector(".button-dir")
+const { open } = window.__TAURI__.dialog;
 
-button.addEventListener("click", function (){
-     QFileDialog::getExistingDirectory(0, "Выбор папки", "");
+function setupButton() {
+  const openDialogBtn = document.getElementById('openDialogBtn');
+  openDialogBtn.addEventListener('click', handleOpenDialogClick);
+}
 
+async function handleOpenDialogClick() {
+  try {
+    const result = await open({
+      multiple: false, // Позволяет выбрать несколько файлов или папок
+      directory: true, // Установите true, чтобы разрешить выбор только папок, а не файлов
+      filter: 'All Files|*.*', // Фильтр файлов (например, 'Images|*.png;*.jpg;*.gif')
+    });
 
-})
+    // result содержит путь(и) выбранных файлов или папок
+    console.log(result);
+  } catch (error) {
+    console.error('Произошла ошибка при открытии диалогового окна: ', error);
+  }
+}
+
+// Вызываем функцию для настройки обработчика кнопки после загрузки DOM
+document.addEventListener('DOMContentLoaded', setupButton);
+
