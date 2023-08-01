55a5c832dcc637dc70d9e0ed5ad25ed0c70c3ee7
kropalikhappines <arsik30394@gmail.com>
2023-08-01 15:19:56 +0300

    Добавил бд, настроил, написал обработчик даты на не существующую будущую дату

---
+++ backend/run.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire application
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:41287@localhost:5432/staticact"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class SubmitForm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    number_camera = db.Column(db.String(50), nullable=False)
+    number_object = db.Column(db.String(15), nullable=False)
+    selected_path = db.Column(db.String(100), nullable=False)
+
+
+def is_valid_date(date_str):
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        return date <= datetime.today()
+    except ValueError:
+        return False
+
+
+@app.route('/api/submit-form', methods=['POST'])
+def submit_form():
+    # submit_table = db.session.execute(db.select(SubmitForm))
+
+    data = request.get_json()
+    if request.method == "POST":
+        if not data["date"] or not is_valid_date(data["date"]):
+            return jsonify({'error': 'Некорректная дата или дата в будущем'}), 400
+
+    forms = SubmitForm(date=data["date"], number_camera=data["numberCamera"],
+                       number_object=data["numberObject"], selected_path=data["selectedPath"])
+    db.session.add(forms)
+    db.session.commit()
+
+    print(data)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
