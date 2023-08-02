1b5806d711a18ba281a0aee23083d2fdd1f0f2af
HATEBTS <93648132+HATEBTS@users.noreply.github.com>
2023-08-02 15:55:00 +0300

    Iron talon (#19)
    * Добавил req
    
    * Обнова
    
    * Добавил settings.py, начал писать функции в main.py, обновил req
    
    * Delete backend directory
    
    * Delete frontend/fileReestr directory
    
    * Delete requirements.txt
    
    * Delete start.py
    
    * внутрянка?
    
    * Delete frontend/fileReestr directory
    
    * Delete requirements.txt
    
    * Пробуем
    
    * req
    
    * Дописал бэк и обработчики, добавлены исключения
    
    ---------
    
    Co-authored-by: kropalikhappines <85121797+kropalikhappines@users.noreply.github.com>
    Co-authored-by: kropalikhappines <arsik30394@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
-import os
 from os import listdir
-from tkinter import filedialog as fd
 import datetime
 import win32con
 import win32api
@@ -8,17 +6,24 @@ import os
 import shutil
 import settings
 
-pat1 = fd.askopenfilename()
-
 
 def rename_sec(path, zv, date):
     files = listdir(path)
     num = 1
     for i in files:
+        f = f"{path}/{zv}_{date}_{num}"
+        if os.path.isfile(f'{f}.mp4'):
+            while os.path.isfile(f'{f}.mp4'):
+                num += 1
+                f = f"{path}/{zv}_{date}_{num}"
+                print(num, f)
         if '.sec' in i:
             full_path = f'{path}/{i}'
+
             file = f"{path}/{zv}_{date}_{num}"
+
             os.rename(full_path, f"{file}.mp4")
+
             win32api.SetFileAttributes(f"{file}.mp4", win32con.FILE_ATTRIBUTE_NORMAL)
             num += 1
 
@@ -29,32 +34,47 @@ def get_video_creation_date(video_path):
         stat_info = os.stat(video_path)
 
         # Извлекаем время создания из статистики и преобразуем в строку
-        creation_time = stat_info.st_ctime
-        creation_date = datetime.datetime.fromtimestamp(creation_time).strftime('%d.%m.%Y')
+        creation_time = stat_info.st_mtime
+        creation_date = datetime.datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d')
 
         return creation_date
 
     except Exception as e:
         print(f"Ошибка при получении даты создания: {e}")
+
         return None
 
 
-def copy_to(serial_number, path, date, cd=settings.DISK):
+def copy_to(path, zv, date, cd=settings.DISK):
+
     file_list = listdir(path)
+
     for file in file_list:
+
         all_path = f"{path}/{file}"
+        print(date, get_video_creation_date(all_path))
+
         if date == get_video_creation_date(all_path):
-            shutil.copyfile(all_path, f'{cd}/{serial_number}')
+            if os.path.isdir(f'{cd}/{zv}') is False:
+                os.mkdir(f"{cd}/{zv}")
+
+            shutil.copy2(all_path, f'{cd}/{zv}/{file}')
 
 
-def main():
-    pass
+def main(dr):
+    try:
+        path = dr["selectedPath"]
+        zv = dr["numberCamera"]
+        date = dr["date"]
 
+        rename_sec(path, zv, date)
 
-if __name__ == "__main__":
-    main()
+        copy_to(path, zv, date)
 
+        return "OK"
 
-print(get_video_creation_date(pat1))
-# rename_sec(fd.askdirectory(), zv='zv', date="date")
+    except Exception as e:
+        print(e)
+
+        return "NO OK"
 
