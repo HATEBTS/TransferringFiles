da1118a561ed41cb899ae54bc54b452f43f372b7
kropalikhappines <arsik30394@gmail.com>
2023-08-07 12:38:31 +0300

    q

--- backend/run.py
+++ backend/run.py
@@ -1,22 +1,19 @@
-from os import environ, makedirs, path
-import io
-import csv
+from os import environ
 
-import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, send_file, render_template
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from datetime import datetime
-from openpyxl import Workbook
 
+from base_disk_xls import unload_disk_base
+from psql_to_xls import psql_to_excel_load
 from main import main
 from settings import BOOL_TRUE, BOOL_FALSE
 
 
 load_dotenv()
-base_path = path.join(path.dirname(__file__), 'data_folder')
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for the entire application
@@ -53,7 +50,6 @@ def is_data_commit(forms, bool):
 
 @app.route('/api/submit-form', methods=['POST'])
 def submit_form():
-    # submit_table = db.session.execute(db.select(SubmitForm))
 
     data = request.get_json()
     forms = SubmitForm(date=data["date"], number_camera=data["numberCamera"],
@@ -64,12 +60,10 @@ def submit_form():
             return jsonify({'date': 'Некорректная дата или дата в будущем'}), 400
 
         if len(data["selectedPath"]) == 0:
-            print(355)
             is_data_commit(forms, BOOL_FALSE)
             return jsonify({'selectedPath': 'Пустой путь'}), 400
 
     fart = main(data)
-    print(fart)
 
     if fart == "NoFile":
         is_data_commit(forms, BOOL_FALSE)
@@ -82,56 +76,17 @@ def submit_form():
     return jsonify(data)
 
 
-db_config = {
-    'dbname': environ.get("DB_NAME"),
-    'user': environ.get("DB_USER"),
-    'password': environ.get("DB_PASS"),
-    'host': environ.get("DB_HOST")
-}
-
-
 @app.route('/download_excel')
 def download_excel():
     if request.method == "GET":
-
-        # Установка соединения с базой данных
-        conn = psycopg2.connect(**db_config)
-        cursor = conn.cursor()
-
-        # Выполнение SQL-запроса для получения данных
-        query = "SELECT * FROM submit_form"
-        cursor.execute(query)
-        data = cursor.fetchall()
-
-        # Создание Excel-файла
-        output = io.BytesIO()
-        workbook = Workbook()
-        sheet = workbook.active
-
-        # Запись данных в Excel
-        for row in data:
-            sheet.append(row)
-
-        # Сохранение в байтовом потоке
-        workbook.save(output)
-        output.seek(0)
-        # excel_folder = r'C:\Users\1\Desktop\last'
-        excel_filename = 'data.xlsx'
-        excel_path = path.join(base_path, excel_filename)
-
-        # Создание папки, если она не существует
-        makedirs(base_path, exist_ok=True)
-
-        with open(excel_path, 'wb') as f:
-            f.write(output.read())
-        # Отправка файла клиенту
-        send_file(
-            io.BytesIO(output.read()),
-            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            as_attachment=True,
-            download_name='data.xlsx'  # Задайте имя файла через заголовок Content-Disposition
-        )
+        psql_to_excel_load()
         return 'Good'
+
+
+@app.route('/base_disk')
+def base_disk():
+    if request.method == "GET":
+        unload_disk_base()
 
 
 if __name__ == "__main__":
--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,12 @@
+from os import environ
+
+
 BOOL_TRUE = True
 BOOL_FALSE = False
+DB_CONFIG = {
+    'dbname': environ.get("DB_NAME"),
+    'user': environ.get("DB_USER"),
+    'password': environ.get("DB_PASS"),
+    'host': environ.get("DB_HOST")
+}
 
