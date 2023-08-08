166a9a7c0593351a930f38eb898fd610e21cd33a
HATEBTS <93648132+HATEBTS@users.noreply.github.com>
2023-08-08 16:12:45 +0300

    Iron talon (#30)
    * Добавил req
    
    * Обнова
    
    * Добавил settings.py, начал писать функции в main.py, обновил req
    
    * Delete backend directory
    
    * Delete frontend/fileReestr directory
    
    * Delete requirements.txt
    
    * Delete start.py
    
    * внутрянка?
    
    * Delete frontend/fileReestr directory
    
    * Delete requirements.txt
    
    * Пробуем
    
    * req
    
    * Дописал бэк и обработчики, добавлены исключения
    
    * мелкие правки
    
    * Добавил пакпки Акты и Видео
    
    * Проверка на Отсутствующие файлы
    
    * Обход
    
    * Обнова огонь, побили файлы, приобрели 100 строк говнокода, ззато все отрабатывает как надо, спасибо!
    
    ---------
    
    Co-authored-by: kropalikhappines <85121797+kropalikhappines@users.noreply.github.com>
    Co-authored-by: kropalikhappines <arsik30394@gmail.com>

--- backend/base_disk_xls.py
+++ backend/base_disk_xls.py
@@ -1,48 +1,97 @@
 from os import walk, path, listdir
-
-paths = r"C:\Users\1\Desktop\last"
-dict_base_path = {
-    "Звено": [],
-    "Номер акта": [],
-    "Статус акта": [],
-    "Статус видео": [],
-    "Кол-во видео": [],
-    "Статус РД": [],
-    "Ссылка на папку": []
-}
-
-
+import openpyxl
+import pandas as pd
+from openpyxl import Workbook
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+dir_count = len(os.environ.get("DISK").split('\\')) + 1
 
 def unload_disk_base(dirs):
+
+    check_list = []
+    dict_base_path = {
+        "Звено": [],
+        "Номер акта": [],
+        "Статус акта": [],
+        "Статус видео": [],
+        "Кол-во видео": [],
+        "Статус РД": [],
+        "Ссылка на папку": []
+    }
     for root, dirs, files in os.walk(os.path.abspath(dirs)):
         for file in files:
-            path_file = os.path.join(root, file).split('\\')
-            zv = path_file[-3][:2]
-            num_act = path_file[-3][:5]
-            # print(num_act)
-            if "Акты" in path_file:
-                pass
-                print(path_file, zv, num_act)
-            else:
-                print(path_file, 'no')
+            print(('_'.join(check_list)), (os.path.join(root, file).split('\\')[dir_count].split('_')[0]))
+            if len(os.path.join(root, file).split('\\')) > dir_count and \
+                    '_' in os.path.join(root, file).split('\\')[dir_count] and \
+                    os.path.join(root, file).split('\\')[dir_count].split('_')[0] not in '_'.join(check_list):
+                path_file = os.path.join(root, file).split('\\')
+                zv = path_file[dir_count].split('_')[0][:2]
+                num_act = path_file[dir_count].split('_')[0][2:5]
+                dict_base_path["Звено"].append(zv)
+                dict_base_path["Номер акта"].append(num_act)
+                list_files = []
+                d = [list_files.extend(files1) for root1, dirs1, files1 in
+                              os.walk(os.path.abspath("/".join(path_file[:dir_count + 1]))) if files1]
+                f = list_files
+                list_files = '_'.join(list_files)
+                if (".xlsx" in list_files or ".xlsm" in list_files or ".XLSX" in list_files or ".XLSM" in
+                        list_files or ".xlsb" in list_files or ".XLSB" in list_files):
+                    dict_base_path["Статус акта"].append(True)
+                    last_akt = True
+                else:
+                    dict_base_path["Статус акта"].append(False)
+                    last_akt = False
+                if '.mp4' in list_files or '.sec' in list_files:
+                    dict_base_path["Статус видео"].append(True)
+                    last_colvid = len([i for i in f if '.mp4' in i or ".sec" in i])
+                    dict_base_path["Кол-во видео"].append(last_colvid)
+                    last_vid = True
+                else:
+                    dict_base_path["Статус видео"].append(False)
+                    last_colvid = 0
+                    dict_base_path["Кол-во видео"].append(0)
+                    last_vid = False
+                if ".pdf" in list_files or ".PDF" in list_files or '.zip' in list_files \
+                        or '.rar' in list_files or '.ZIP' in list_files or '.RAR' in list_files:
+                    dict_base_path["Статус РД"].append(True)
+                    last_rd = True
+                else:
+                    dict_base_path["Статус РД"].append(False)
+                    last_rd = False
+                dict_base_path["Ссылка на папку"].append("/".join(str(root).split("\\")[0:-1]))
+                if "," in os.path.join(root, file).split('\\')[dir_count]:
+                    check_list.append(path_file[dir_count].split('_')[0])
+                    prost = os.path.join(root, file).split('\\')[dir_count].split('_')[0].split(',')
+                    for _ in range(1, len(prost)):
+                        dict_base_path["Звено"].append(prost[_][:2])
+                        dict_base_path["Номер акта"].append(prost[_][2:5])
+                        dict_base_path["Статус акта"].append(last_akt)
+                        dict_base_path["Статус видео"].append(last_vid)
+                        dict_base_path["Кол-во видео"].append(last_colvid)
+                        dict_base_path["Статус РД"].append(last_rd)
+                        dict_base_path["Ссылка на папку"].append("/".join(str(root).split("\\")[0:-1]))
+                        check_list.append(prost[_])
+                else:
+                    check_list.append(path_file[dir_count].split('_')[0])
+    return dict_base_path
+def svod(paths=os.environ.get("DISK")):
+    try:
+        d = unload_disk_base(paths)
+        for i in d.values():
+            print(len(i))
+        writer = openpyxl.Workbook()
+        writer.save("Sozdal.xlsx")
+        writer.close()
 
-            # if "Видео" in path_file:
-            #     print(path_file, zv, num_act)
-
-
-
-print(unload_disk_base(paths))
-
-# >>> df = pd.DataFrame({
-# ...     'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'],
-# ...     'population': [17.04, 143.5, 9.5, 45.5],
-# ...     'square': [2724902, 17125191, 207600, 603628]
-# ... })
-# >>> df
-#    country  population    square
-# 0  Kazakhstan       17.04   2724902
-# 1      Russia      143.50  17125191
-# 2     Belarus        9.50    207600
-# 3     Ukraine       45.50    603628
+        writer = pd.ExcelWriter('Sozdal.xlsx')
+        df_marks = pd.DataFrame(d)
+        df_marks.to_excel(writer)
+        writer._save()
+        return "K KOK"
+    except Exception as e:
+        print(e)
+        return  "WHAT"
