15577e313e38dc7dbc953a6a5a8b2fadd94dc0e4
kropalikhappines <arsik30394@gmail.com>
2023-08-14 17:03:18 +0300

    conflict

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,18 +4,8 @@ import DirectorySelectDialog from "./FileDialogButton";
 import CheckBox from "./StateUseCheckbox";
 import LoadingModal from "./ModalLoading";
 import React, {useState} from "react";
-<<<<<<< HEAD
 import ElementsTechnique from "./ElementsTechnique";
-import InfoTime from "./InfoTime";
-=======
 
-const handleDirectorySelect = (selectedPath) => {
-  console.log('Selected directory path:', selectedPath);
-  const selectedPathInput = document.getElementById('selectedPath');
-    selectedPathInput.value = selectedPath;
-  // Do whatever you want to do with the selected path here
-};
->>>>>>> d98c2561c024c43ff04aceff0d78677d435b3719
 
 
 const HomePages = (event) => {
@@ -69,21 +59,9 @@ const HomePages = (event) => {
                         <div className={"input-group mb-3"}>
                             <CheckBox />
 
-<<<<<<< HEAD
                         </div>
                     </div>
-=======
-                        <option>Звено 1</option>
-                        <option>Звено 2</option>
-                        <option>Звено 3</option>
-                        <option>Звено 4</option>
-                        <option>Звено 5</option>
-                        <option>Звено 6</option>
-                        <option>Звено 7</option>
-                        <option>Звено 8</option>
-                        <option>Звено 9</option>
-                        <option>Звено 10</option>
->>>>>>> d98c2561c024c43ff04aceff0d78677d435b3719
+
 
                     <div className={"row"}>
                         <p className={"mt-5 w-25 mx-auto"}><input className={"btn btn-outline-secondary"} id={"btn"} type={"submit"}/></p>
