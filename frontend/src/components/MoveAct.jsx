cd470ca5f51d3e19f7f345c8b9a6f95dd6fefaa3
HATEBTS <93648132+HATEBTS@users.noreply.github.com>
2023-08-15 10:30:26 +0300

    Knopki2

---
+++ frontend/src/components/MoveAct.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ApiCallButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleApiCall = async () => {
+    try {
+      setIsLoading(true);
+
+      // Вызов API
+      const response = await axios.get('http://127.0.0.1:5000/move_act');
+
+      // Обработка ответа от API
+      console.log('Ответ от API:', response.data);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Ошибка вызова API:', error);
+      alert("Ошибка", error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+        <div className={"row align-items-start"}>
+          <div className={"col"}>
+            <button className={"btn btn-dark"} onClick={handleApiCall} disabled={isLoading}>
+              {isLoading ? 'Загрузка...' : 'Раскидать акты'}
+            </button>
+          </div>
+        </div>
+  );
+};
+
+export default ApiCallButton;
